2a4bed21017c93e2181f83064dae08bc1402f4d8
Rafael Pinto <108489894+rafaelpinto1@users.noreply.github.com>
2025-05-30 10:52:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function acessar(tipo) {
+  if (tipo === "funcionario") {
+    window.location.href = "funcionario.html"; // substitua pelo caminho real
+  } else if (tipo === "cliente") {
+    window.location.href = "cliente.html"; // substitua pelo caminho real
+  }
+}
